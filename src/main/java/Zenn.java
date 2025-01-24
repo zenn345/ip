514ae586a80fec7356a34968f92a4ee5111e0dac
Zenn Saw <>
2025-01-24 15:10:26 +0800

    add Zenn.java

---
+++ src/main/java/Zenn.java
@@ -0,0 +1,12 @@
+public class Zenn {
+    public static void main(String[] args) {
+        String logo = " ____ ____ _    _ _    _\n"
+                + "|_ _ |  __| \\  | | \\  | |\n"
+                + "  / /| |__|  \\ | |  \\ | |\n"
+                + " / / |  __|   \\| |   \\| |\n"
+                + "/ /_ | |__| |\\   | |\\   |\n"
+                + "|____|____|_| \\ _|_| \\ _|\n";
+        System.out.println("Hello! I'm Zenn\n" + logo + "\n How may I help you today?"
+        + "\n \n Bye. Hope to see you again soon!");
+    }
+}

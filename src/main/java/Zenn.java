6d9028309e65350ef058c420905e1a8d3c4d9450
Zenn Saw <>
2025-02-12 16:20:08 +0800

    Added Task.java and change Zenn.java accordingly

--- src/main/java/Zenn.java
+++ src/main/java/Zenn.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
 public class Zenn {
-    public static void main(String[] args) {
+    private static int taskCount = 0;
+    private static final int MAX_TASKS = 100;
+    private static Task[] tasks = new Task[MAX_TASKS];
+
+    public static void main(String[] args) {;
         String logo = " ____ ____ _    _ _    _\n"
                 + "|_ _ |  __| \\  | | \\  | |\n"
                 + "  / /| |__|  \\ | |  \\ | |\n"
@@ -10,27 +14,42 @@ public class Zenn {
         System.out.println("Hello! I'm Zenn\n" + logo + "\n How may I help you today?");
 
         Scanner scanner = new Scanner(System.in);
-        String[] history = new String[100];
-        int taskCount = 0;
-        String command;
 
         while (true) {
             System.out.println(">");
-            command = scanner.nextLine();
+            String input = scanner.nextLine();
 
-            if (command.equalsIgnoreCase("bye")) {
+            if (input.equalsIgnoreCase("bye")) {
                 System.out.println("Bye bye! See you soon!");
                 break;
-            } else if (command.equalsIgnoreCase("list")) {
-                System.out.println("Here's your todo list:");
+            } else if (input.equalsIgnoreCase("list")) {
+                System.out.println("Here's your todo list");
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println((i + 1) +". " + history[i]);
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+            } else if (input.startsWith("mark ")) {
+                int index = Integer.parseInt(input.split(" ")[1]) - 1;
+                if (index >= 0 && index < taskCount) {
+                    tasks[index].markAsDone();
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println("  " + tasks[index]);
+                } else {
+                    System.out.println("Invalid task number.");
+                }
+            } else if (input.startsWith("unmark ")) {
+                int index = Integer.parseInt(input.split(" ")[1]) - 1;
+                if (index >= 0 && index < taskCount) {
+                    tasks[index].unmarkAsDone();
+                    System.out.println("OK, I've marked this task as not done yet:");
+                    System.out.println("  " + tasks[index]);
+                } else {
+                    System.out.println("Invalid task number.");
                 }
             } else {
-                if (taskCount < 100) {
-                    history[taskCount] = command;
+                if (taskCount < MAX_TASKS) {
+                    tasks[taskCount] = new Task(input);
                     taskCount++;
-                    System.out.println("You've added: " + command);
+                    System.out.println("You've added: " + input);
                 }
             }
         }
